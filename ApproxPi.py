3286754d5ec77b1dc1f64397d24b97386bdd3b9e
RandiMorrow <morrow.randi@yahoo.com>
2025-02-08 18:10:25 +0000

    Done with lab

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,6 +12,7 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  number = int(input("Enter a decimal precision between 1 and 10: "))
 
   start = time.time()
   #calculate pi using the approximation technique
@@ -19,12 +20,18 @@ def main():
   approxPi = 4/1
   sign = -1
   denom = 3
-  while round(approxPi, 3) != round(realPi, 3): 
-      print(approxPi)
-      approxPi = approxPi + (sign * 4 / denom) 
 
-      sign = sign * -1 
-      denom = denom + 2
+  while True:
+    new_approxPi = round(approxPi, number)
+    new_realPi = round(realPi, number) 
+
+    if new_approxPi == new_realPi:
+      break
+
+    approxPi = approxPi + (sign * 4 / denom) 
+    sign = sign * -1 
+    denom = denom + 2
+
 
   end = time.time()
 
